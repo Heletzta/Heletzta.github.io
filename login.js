f96bb62c18f86c5310ab9fea4d235b7498624d19
Elsa Abbas <eabbas@berkeley.edu>
2024-07-30 11:13:55 -0700

    Trying something

--- login.js
+++ login.js
@@ -1,11 +1,11 @@
 function setInputError(inputElement, message) {
-    inputElement.classList.add("form__input--error")
+    inputElement.classList.add("form__input--error");
     inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
 
 }
 
 function clearInputError(inputElement) {
-    inputElement.classList.remove("form__input--error")
+    inputElement.classList.remove("form__input--error");
     inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
 }
 
@@ -14,7 +14,7 @@ function setFormMessage(formElement, type, message) {
     const messageElement = formElement.querySelector(".form__message");
 
     messageElement.textContent = message;
-    messageElement.classList.remove("form__mesaage--success", "form__message--error")
+    messageElement.classList.remove("form__mesaage--success", "form__message--error");
     messageElement.classList.add('form__message--${type}');
 }
 
@@ -38,14 +38,14 @@ document.addEventListener("DOMContentLoaded", () => {
     loginForm.addEventListener("submit", e => {
         e.preventDefault();
 
-        setFormMessage(loginForm, "error", "Invalid username/ password combination")
+        setFormMessage(loginForm, "error", "Invalid username/ password combination");
 
     });
 
     document.querySelectorAll(".form__input").forEach(inputElement => {
         inputElement.addEventListener("blur", e => {
             if (e.target.id == "SignUpUsername" && e.target.value.length > 0 && e.target.value.length < 7)
-            setInputError(inputElement, "Username must be at least 7 characters in length")
+            setInputError(inputElement, "Username must be at least 7 characters in length");
         });
 
         inputElement.addEventListener("input", e=> {
