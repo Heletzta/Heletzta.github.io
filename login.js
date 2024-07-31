5826872946e1ef4a5c8bed6fd42dceca51fc9237
Elsa Abbas <eabbas@berkeley.edu>
2024-07-30 17:42:59 -0700

    Fixing login

--- login.js
+++ login.js
@@ -21,7 +21,7 @@ function setFormMessage(formElement, type, message) {
 
 document.addEventListener("DOMContentLoaded", () => {
     const loginForm = document.querySelector("#login");
-    const createAccountForm = document.querySelector("#login");
+    const createAccountForm = document.querySelector("#createAccount");
 
     document.querySelector("#linkCreateAccount").addEventListener("click", e => {
         e.preventDefault();
